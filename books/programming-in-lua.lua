1d8ddb9bce6ae3f55125851563386bfb5b6679f7
Johan-Madronero-Cuervo <jmadronero@unal.edu.co>
2023-08-01 23:06:36 -0500

    Leí el primer capítulo de Lua.

--- books/programming-in-lua.lua
+++ books/programming-in-lua.lua
@@ -71,7 +71,7 @@ Chapter 1. Exercises
 -- enter a negative number?
 -- Modify the example to avoid this problem.
 
----[[
+--[[
 -- A negative number results in a stack traceback error.
 -- We can easily fix this error restricting the value of n.
 function fact (n)
@@ -86,3 +86,62 @@ print("Enter a number:")
 a = io.read("*n")
 print(fact(a))
 --]]
+
+-- Exercise 1.2: Run the twice example, both by loading the file with the -l
+-- option and with dofile. Which way do you prefer?
+
+-- [[
+-- I prefer the dofile option
+-- ]]
+
+-- Exercise 1.3: Can you name other languages that use "--" for comments?
+
+--[[
+-- I do not know of any other language that use "--" for comments.
+-- A quick search shows up Ada, Applescript, Eiffel, Haskell, SQL and VHDL.
+--]]
+
+-- Exercise 1.4: Which of the following strings are valid identifiers?
+
+--[[
+  ___
+  _end
+  End
+  nil
+  NULL
+
+--]]
+
+-- Exercise 1.5: What is the value of the expression type(nil) == nil
+
+--[[
+print(type(nil) == nil) --> false
+--]]
+
+-- Exercise 1.6: How can you check whether a value is a Boolean without using
+-- the function type?
+
+--[[
+-- I would do:
+local value = true
+print(value == true)
+--]]
+
+-- Exercise 1.7: Are the parentheses necessary? Would you recommend their use in
+-- that expression?
+
+--[[
+-- I would, since it improves readability.
+--]]
+
+-- Exercise 1.8: Write a simple script that prints its own name without knowing
+-- it in advance.
+
+--[[
+print(arg[0]) -- No text formatting due to experience.
+--]]
+
+--[[
+Chapter 2. Interlude: The Eight-Queen Puzzle
+]]
+
