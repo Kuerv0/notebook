9dfe45c51544d93497896ac9678957dbcba2c1d3
johan <jmadronero@unal.edu.co>
2023-08-01 20:21:12 -0500

    Avance en libro y manual de usuario.

--- books/programming-in-lua.lua
+++ books/programming-in-lua.lua
@@ -44,9 +44,45 @@ print(fact(a))
 -- A comment in Lua.
 
 --[[
-A multile comment in Lua.
-]]
+-- A multile comment in Lua.
+--]]
 
 -- To reactivate the code, we add a single hyphen to the first line.
 
 -- Global variables do not need declarations.
+
+-- The function type gives the type of any given value:
+-- type(true) --> boolean
+
+--[[
+type(true) --> boolean
+--]]
+
+-- We can assign nil to a global variable to delete it!
+
+-- x = x or v is equivalent to: 
+-- if not x then x = v end
+
+--[[
+Chapter 1. Exercises
+]]
+
+-- Exercise 1.1: Run the factorial example. What happens to your program if you
+-- enter a negative number?
+-- Modify the example to avoid this problem.
+
+---[[
+-- A negative number results in a stack traceback error.
+-- We can easily fix this error restricting the value of n.
+function fact (n)
+  if n <= 0 then -- n must be greater than or equal to 0 
+    return 1
+  else
+    return n * fact(n - 1)
+  end
+end
+
+print("Enter a number:")
+a = io.read("*n")
+print(fact(a))
+--]]
