ea1d52f39f5613fd66226af8d10c203e015f5b73
Johan-Madronero-Cuervo <jmadronero@unal.edu.co>
2023-09-22 16:12:47 -0500

    Inicio libro, avance AWS
    Fri Sep 22 16:13:02 2023
    Hoy empecé a estudiar seriamente para el parcial de AWS y también empecé
    a leer un libro de C++.

---
+++ books/modern-c++-programming-cookbook.cpp
@@ -0,0 +1,25 @@
+/*
+ * Title: Modern C++ Programming Cookbook 2nd Edition
+ * Author: Marius Bancila
+*/
+
+#include <initializer_list>
+int main (int argc, char *argv[]) {
+  
+  return 0;
+}
+
+/*
+ * 1 Learning Modern Core Language Features
+*/
+
+/*
+ * Using auto whenever possible
+*/
+
+void UsingAuto(){
+  auto i = 42;
+  auto d = 42.5;
+  auto s = "text";
+  auto v = {1, 2, 3};
+}
